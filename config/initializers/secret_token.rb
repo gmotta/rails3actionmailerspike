95df8c39ab3d3d907a6af294a3ce1248571087ac
Motta <gfcmotta@gmail.com>
2011-03-04 14:25:29 -0300

    first commit with the spike using gmail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailspike::Application.config.secret_token = '9956e035867472c1c1676812d3438b7f20edfb7361bc8f522e28a2f70a7614a27bed1a700ea12550f0ea130c8f1adb4b674979703279f628704320033ac0f0e9'
